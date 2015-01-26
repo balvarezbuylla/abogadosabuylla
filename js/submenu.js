c0fce67f4e2b5063230b686b21cc78cbee41d70a
Beatriz <balvarezbuylla@gmail.com>
2015-01-26 18:47:44 +0100

    initial commit

---
+++ js/submenu.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+$('ul.submenu').each(function(e) {  
+  
+  /* left des aktuellen Elements */  
+  l = $(this).css('left');  
+  pl = $(this).parent().position(); 
+  //alert(l+' --- '+pl.left);
+  
+  /* Wenn Höhe erfolgreich bestimmt werden konnte */  
+  if(pl != "") {  
+  pos = pl.left;
+  pos = pos - (2*pos);
+  pos = pos+1;
+  $(this).css('left',pos+'px');
+  }  
+});  
+});
+
